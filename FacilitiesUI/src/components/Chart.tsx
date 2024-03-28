10b0aaf271470a696d8605d73c07ea332cd6047a
amaroguy <amaroguy@gmail.com>
2024-03-27 20:41:13 -0700

    Everything works!

--- FacilitiesUI/src/components/Chart.tsx
+++ FacilitiesUI/src/components/Chart.tsx
@@ -1,6 +1,7 @@
 import { UseQueryResult } from "@tanstack/react-query"
 import { FacilityRecord } from "../types/types"
 import { facilityRecordsToChartJSData } from "../utils"
+import { Bar, Line } from "react-chartjs-2"
 
 interface ChartProps {
     data: UseQueryResult<FacilityRecord[], Error>[]
@@ -18,8 +19,10 @@ export const FacilityChart = ({data, isChartDataLoading}:ChartProps) => {
 
     console.log(chartData, "THIS IS THE CHART DATA")
 
-    const datasets = chartData.map((records) => facilityRecordsToChartJSData(records as FacilityRecord[]))
+    const datasets = chartData.map((records) => ({data: facilityRecordsToChartJSData(records as FacilityRecord[])}))
 
     console.log("DONEEEEE", datasets)
+
+    return <Line data={{datasets}}/>
 
 }
--- FacilitiesUI/src/main.tsx
+++ FacilitiesUI/src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { CategoryScale, Chart, LinearScale, BarElement, PointElement, LineElement } from 'chart.js'
 
 const queryClient = new QueryClient()
+Chart.register(CategoryScale)
+Chart.register(LinearScale)
+Chart.register(BarElement)
+Chart.register(PointElement)
+Chart.register(LineElement)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
